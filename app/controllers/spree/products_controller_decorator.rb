ae689a5a46a6e3aed1046397dd64d3d30115e7b1
brijesh varshney <brijesh.varshney@w3villa.com>
2017-03-01 13:59:14 +0530

    fixed facet via selecting checkbox

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -7,10 +7,10 @@ module Spree
 				@products = Sunspot.search(Spree::Product) do
 					if params[:q] && params[:q][:product_property_name]
 						params[:q][:product_property_name].each do |product_property_name|
-				          with(:product_property_name, product_property_name[1])
-				        end
+		          with(:product_property_name, product_property_name[1])
+		        end
 					end	
-					paginate(:page => params[:page].blank? ? 1 : params[:page], :per_page => 15)
+					paginate(:page => params[:page].blank? ? 1 : params[:page], :per_page => 12)
 				end.results
 	      		@taxonomies = Spree::Taxonomy.includes(root: :children)
 			render 'spree/products/solr_search'
@@ -22,10 +22,10 @@ module Spree
 				p '#'*100
 				@facet = Sunspot.search(Spree::Product) do
 					dynamic(:product_property_ids) do
-				p '====================================='
-				@facet_pro = Sunspot.search(Spree::Product) do
-					fulltext "*#{params[:search]}*"
-				end.results
+						p '====================================='
+						@facet_pro = Sunspot.search(Spree::Product) do
+							fulltext "*#{params[:search]}*"
+						end.results
 			
 		        @facet_pro.collect{|x| x.properties.where(filterable: true)}.flatten.uniq.each do |property|
 		          facet(property.id)
@@ -36,14 +36,11 @@ module Spree
 			else
 				p '@'*100
 				@facet = Sunspot.search(Spree::Product) do
-					if params[:q] && params[:q][:product_property_name]
-						params[:q][:product_property_name].each do |product_property_name|
-				        with(:product_property_name, product_property_name[1])
-				     end
-					end	
-					unless params[:search]
-						 fulltext "*#{params[:search]}*"
-					end
+					# if params[:q] && params[:q][:product_property_name]
+					# 	params[:q][:product_property_name].each do |product_property_name|
+			  #       with(:product_property_name, product_property_name[1])
+			  #   	end
+					# end	
 						dynamic(:product_property_ids) do
 			        Spree::Property.where(filterable: true).each do |property|
 			          facet(property.id)
